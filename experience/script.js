21297ab7df408c5dd7f8dd927361e370c7e24007
Somojit Banerjee <somojitb@gmail.com>
2026-07-18 15:18:58 -0600

    Refresh portfolio content and animations

--- experience/script.js
+++ experience/script.js
@@ -29,6 +29,17 @@ const srtop = ScrollReveal({
 srtop.reveal('.experience .timeline',{delay: 400});
 srtop.reveal('.experience .timeline .container',{interval: 400}); 
 
+if (typeof anime !== 'undefined' && !window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+    anime({
+        targets: '.experience .content',
+        translateY: [28, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(120),
+        duration: 850,
+        easing: 'easeOutCubic'
+    });
+}
+
 
 // Start of Tawk.to Live Chat
 
@@ -45,30 +56,11 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 // End of Tawk.to Live Chat
 
 
-// disable developer mode
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
-
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Saurav Mukherjee";
-        $("#favicon").attr("href","/assests/images/favicon.png");
+        document.title = "Experience | Portfolio Somojit Banerjee";
+        $("#favicon").attr("href","/assests/images/favicon.jpg");
     }
     else {
         document.title = "Come Back To Portfolio";
